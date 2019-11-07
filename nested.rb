bc55653c5230a73bcf67ac966bbbbe604e53f610
Francesca Dreith <francesca.dreith@gmail.com>
2019-11-07 21:40:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+ puts programmer_hash[:yukihiro_matsumoto]
     
 end
 
